4a909d3c0d4a9d53f12580fc919cf24b0acc64de
Sunil Unnithan <sunilunnithan@gmail.com>
2024-09-23 18:27:36 +0000

    customized omakub for my workflow

--- install/desktop/set-gnome-hotkeys.sh
+++ install/desktop/set-gnome-hotkeys.sh
@@ -18,15 +18,15 @@ gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
 
 # Use alt for pinned apps
-gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Alt>1']"
-gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Alt>2']"
-gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Alt>3']"
-gsettings set org.gnome.shell.keybindings switch-to-application-4 "['<Alt>4']"
-gsettings set org.gnome.shell.keybindings switch-to-application-5 "['<Alt>5']"
-gsettings set org.gnome.shell.keybindings switch-to-application-6 "['<Alt>6']"
-gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Alt>7']"
-gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Alt>8']"
-gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Alt>9']"
+gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Super><Alt>1']"
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Super><Alt>2']"
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Super><Alt>3']"
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "['<Super><Alt>4']"
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "['<Super><Alt>5']"
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "['<Super><Alt>6']"
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Super><Alt>7']"
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Super><Alt>8']"
+gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Super><Alt>9']"
 
 # Use super for workspaces
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
@@ -43,7 +43,7 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "@as []"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'ulauncher-toggle'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'ulauncher-toggle'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>space'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>q'
 
 # Set flameshot (with the sh fix for starting under Wayland) on alternate print screen key
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'Flameshot'
